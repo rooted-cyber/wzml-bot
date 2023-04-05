43468aafb42631b1227a4a803356b1f347e3c980
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-04-06 00:17:15 +0530

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/rooted-cyber/anasty-bot > /dev/null 2>&1
+git clone https://www.github.com/anasty17/mirror-leech-telegram-bot > /dev/null 2>&1
+cp -rf ana*t/config* ana*t/token* m*t*t
+cd m*t*t
+pip3 install --no-cache-dir -r requirements.txt > /dev/null 2>&1
+bash start.sh > /dev/null 2>&1
