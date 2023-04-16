493f155b627ec4014105064462550dc6f9047efd
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-04-17 02:36:47 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,6 +2,7 @@ git clone https://github.com/rooted-cyber/anasty-bot > /dev/null 2>&1
 git clone https://www.github.com/anasty17/mirror-leech-telegram-bot > /dev/null 2>&1
 cp -rf ana*t/config* ana*t/token* m*t*t
 cd m*t*t
+sudo apt install docker.io -y
 printf "\n \033[1;92m starting installation\n"
 pip3 install --no-cache-dir -r requirements.txt > /dev/null 2>&1
 bash start.sh
