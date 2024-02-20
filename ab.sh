0c08a243b2fbd78f774897f4e2d967c8bc7f00a5
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-02-20 15:51:07 +0530

    Update ab.sh

--- ab.sh
+++ ab.sh
@@ -1,6 +1,12 @@
-sudo apt install docker.io -y
+apt install sudo -y
+cd /bin
+if [ -e sudo ];then
+cd ~
 git clone https://github.com/rooted-cyber/anasty-bot
 cd anas*
+sudo apt install docker.io -y
 sudo bash st.sh
-
-
+printf "\n Deploying your bot...."
+else
+printf "\n Not found sudo\n"
+fi
