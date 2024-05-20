10d169d10a87c445bb7133e07d02b3df7933b4e0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-05-20 18:44:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+ru() {
+sudo apt update && sudo apt upgrade -y
+sudo apt install docker.io wget -y
+wget https://github.com/rooted-cyber/anasty-bot/raw/main/Dockerfile
+sudo docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
+}
 cd /bin
 if [ -e sudo ];then
 sudo apt update && sudo apt upgrade -y
@@ -9,6 +15,7 @@ sudo chmod -R 777 a
 cd a
 git pull
 sudo bash st.sh
+ru
 else
 printf "\n without sudo not running \n\n"
 exit
