5c61d136f72e838dc622d1add41c051c354b9e89
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-07-10 00:24:02 +0530

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,2 +1,2 @@
 sudo apt install docker.io -y
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
+sudo docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
