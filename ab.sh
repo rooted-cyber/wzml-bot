ec0e353023d39daad7aed0d88087bea093245fea
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-07-28 20:04:25 +0530

    Update ab.sh

--- ab.sh
+++ ab.sh
@@ -2,34 +2,5 @@ apn() {
 apt install sudo git -y
 apt install docker.io -y
 }
-apn > /dev/null 2>&1
-dpb() {
-cd /bin
-if [ -e sudo ];then
-cd ~
-git clone https://github.com/rooted-cyber/anasty-bot
-cd anas*
-ls
-printf "\n Deploying your bot....\n"
-sudo bash st.sh
-printf "\n Deployed your bot....\n"
-else
-printf "\n Not found sudo\n"
-fi
-}
-chs() {
-cd /bin
-if [ -e sudo ];then
-cd ~/anasty*
-ls
-printf "\n Deploying your bot....\n"
-sudo bash st.sh
-printf "\n Deployed your bot....\n"
-fi
-}
-if [ -e anasty-bot ];then
-cd anasty*
-chs
-else
-dpb
-fi
+command -v docker || apn
+sudo docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
